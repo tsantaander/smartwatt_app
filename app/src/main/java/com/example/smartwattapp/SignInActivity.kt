11fd6ecc19aca7c9f30ae5048653c510721fd775
diegitfk <cancinod080gmail.com>
2024-10-19 14:26:48 -0300

    Correción de referencias de strings completas

--- app/src/main/java/com/example/smartwattapp/SignInActivity.kt
+++ app/src/main/java/com/example/smartwattapp/SignInActivity.kt
@@ -1,27 +1,21 @@
 package com.example.smartwattapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.siginbtn);
-        button.setOnClickListener {
-            val intent = Intent(this , SignInActivity::class.java)
-            startActivity(intent)
-        }
+
     }
 }
